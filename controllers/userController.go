622b8bfbee04ef4e23c24da77cdb862c7904cf34
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2023-09-26 10:35:56 +0530

    helper

--- controllers/userController.go
+++ controllers/userController.go
@@ -63,7 +63,19 @@ func Signup() gin.HandlerFunc {
 		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.ID = primitive.NewObjectID()
-		user.User_id = user.ID
+		user.User_id = user.ID.Hex()
+		token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name. *user.Last_name, *user.User_type, *&user.User_id)
+		user.Token = &token
+		user.Refresh_token = &refreshToken
+
+		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+		if insertErr!=nil{
+			msg:= fmt.Sprintf("User item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error":msg})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOk, resultInsertionNumber)
 
 	}
 }
